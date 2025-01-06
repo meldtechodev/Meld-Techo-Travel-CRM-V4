358fbcf3d4ddc246d8a347ddf4dd4499ff4e051f
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2025-01-06 12:26:52 +0530

    cross origin added

--- src/main/java/com/meld/techo/travel/crm/controller/AuthController.java
+++ src/main/java/com/meld/techo/travel/crm/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -26,6 +27,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("Motherson/crm/v1")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class AuthController {
 	 @Autowired
 	    private UserRepository userRepository;
